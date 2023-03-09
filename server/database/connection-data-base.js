7b578103abf9f57f478d962116a0f87954dd4efb
LucasAlamelou <lucas.alamelou@gmail.com>
2023-03-09 20:39:12 +0100

    server: database: ajout fonction récuperation données requête SQL

--- server/database/connection-data-base.js
+++ server/database/connection-data-base.js
@@ -28,3 +28,74 @@ export async function getSomething() {
     console.log(result);
     return result;
 }
+
+/**
+ * Rêquete pour trouver l'user par id
+ * @param {Number} idUser
+ * @returns {object} user
+ */
+export async function getUserById(idUser) {
+    const [result] = await pool.query('SELECT * FROM user WHERE user.id = ?', [idUser]);
+    return result;
+}
+
+/**
+ * Rêquete pour trouver l'user par email
+ * @param {String} emailUser
+ * @returns {object} user
+ */
+export async function getUserByEmail(emailUser) {
+    const [result] = await pool.query('SELECT * FROM user WHERE user.email = ?', [emailUser]);
+    return result[0];
+}
+
+/**
+ * Rêquete rentourne tout les lignes des tables associées au membre par son id
+ * Filtre : aucun
+ * @param {Number} idMember
+ * @returns {object} membre complet
+ */
+export async function getMemberById(idMember) {
+    const [result] = await pool.query(
+        'SELECT * FROM member m INNER JOIN performanceMember pm ON pm.memberId = m.id INNER JOIN healthMember hm ON hm.memberId = m.id WHERE m.id = ? ',
+        [idMember]
+    );
+    return result[0];
+}
+
+/**
+ * Rêquete rentourne tout les lignes des tables associées associées au training par id du membre
+ * Filtre : aucun
+ * @param {Number} idMember
+ * @returns {Array}
+ */
+export async function getAllDataTrainingByMemberId(idMember) {
+    const [result] = await pool.query(
+        'SELECT * FROM training t INNER JOIN typeOfTraining tp ON tp.id = t.idTypeOfTraining INNER JOIN metricTraining mt ON mt.idTraining = t.id INNER JOIN metricHealthTraining mht ON mht.idTraining = t.id INNER JOIN metricOptionalTraining mot ON mot.idTraining = t.id WHERE t.idMember = ? ',
+        [idMember]
+    );
+    console.log(result);
+    return result;
+}
+
+/**
+ * Rêquete rentourne les trainings du membre
+ * Filtre : aucun
+ * @param {Number} idTraining
+ * @returns {Array} training par id du membre
+ */
+export async function getAllTrainingByMemberId(idMember) {
+    const [result] = await pool.query('SELECT * FROM training t WHERE t.idMember = ? ', [idMember]);
+    return result;
+}
+
+/**
+ * Rêquete rentourne le training uniquement
+ * Filtre : aucun
+ * @param {Number} idTraining
+ * @returns {Array} row training par id
+ */
+export async function getTrainingById(idTraining) {
+    const [result] = await pool.query('SELECT * FROM training t WHERE t.id = ? ', [idTraining]);
+    return result[0];
+}
